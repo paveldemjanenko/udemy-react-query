45b3d796d6058c5518c90dfff14b22b7ecefec0b
Pavels Demjanenko <pavel.demjanenko@gmail.com>
2022-10-22 12:08:04 +0300

    base-blog-em useQuery, isLoading, isError, QueryKeys, Pagination, Pre-fetching Data

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,16 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, error, isError } = useQuery(['comments', post.id], () => fetchComments(post.id));
+ 
+  if (isLoading) return <h3>Loading...</h3>;
+  
+  if (isError) return (
+    <>
+      <h3>Oop, something went wrong</h3>
+      <p>{error.toString()}</p>
+    </>
+  );
 
   return (
     <>

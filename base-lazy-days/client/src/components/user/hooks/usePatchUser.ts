376ec69cd9544d23e7e7425abdb67662c8471e6d
Pavels Demjanenko <pavel.demjanenko@gmail.com>
2022-10-28 09:54:56 +0300

    Optimistic update

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,37 +1,86 @@
 import jsonpatch from 'fast-json-patch';
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import { useCustomToast } from '../../app/hooks/useCustomToast';
 import { useUser } from './useUser';
 
 // for when we need a server function
-// async function patchUserOnServer(
-//   newData: User | null,
-//   originalData: User | null,
-//  ): Promise<User | null> {
-//   if (!newData || !originalData) return null;
-//   // create a patch for the difference between newData and originalData
-//   const patch = jsonpatch.compare(originalData, newData);
+async function patchUserOnServer(
+  newData: User | null,
+  originalData: User | null,
+): Promise<User | null> {
+  if (!newData || !originalData) return null;
+  // create a patch for the difference between newData and originalData
+  const patch = jsonpatch.compare(originalData, newData);
 
-//   // send patched data to the server
-//   const { data } = await axiosInstance.patch(
-//     `/user/${originalData.id}`,
-//     { patch },
-//     {
-//       headers: getJWTHeader(originalData),
-//     },
-//   );
-//   return data.user;
-//  }
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
+    `/user/${originalData.id}`,
+    { patch },
+    {
+      headers: getJWTHeader(originalData),
+    },
+  );
+  return data.user;
+}
 
-// TODO: update type to UseMutateFunction type
-export function usePatchUser(): (newData: User | null) => void {
+export function usePatchUser(): UseMutateFunction<
+  User,
+  unknown,
+  User,
+  unknown
+> {
   const { user, updateUser } = useUser();
+  const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
-  // TODO: replace with mutate function
-  const patchUser = (newData: User | null) => {
-    // nothing to see here
-  };
+  const { mutate: patchUser } = useMutation(
+    (newUserData: User) => patchUserOnServer(newUserData, user),
+    {
+      // onMutate returns context that is passed to onError
+      onMutate: async (newData: User | null) => {
+        // cancel any outgoing queris for user data, old server data
+        // doesn't overwrite our optimistic update
+        queryClient.cancelQueries([queryKeys.user]);
+
+        // snapshot of previous user value
+        const previouseUserData: User = queryClient.getQueryData(
+          queryKeys.user,
+        );
+
+        // optimisticaly update the cache eith new value
+        updateUser(newData);
+
+        // return context object with snapshotted value
+        return { previouseUserData };
+      },
+      onError: (error, newData, context) => {
+        // roll back cache to saved value
+        if (context.previouseUserData) {
+          updateUser(context.previouseUserData);
+          toast({
+            title: 'Update failed; restoring pervoius values',
+            status: 'warning',
+          });
+        }
+      },
+      onSuccess: (userData: User | null) => {
+        if (userData) {
+          toast({
+            title: 'User updated!',
+            status: 'success',
+          });
+        }
+      },
+      onSettled: () => {
+        // invalidate use query to make sure we're in sync with server data
+        queryClient.invalidateQueries(queryKeys.user);
+      },
+    },
+  );
 
   return patchUser;
 }
--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -10,12 +10,16 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-async function getUser(user: User | null): Promise<User | null> {
+async function getUser(
+  user: User | null,
+  signal: AbortSignal,
+): Promise<User | null> {
   if (!user) return null;
   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
     `/user/${user.id}`,
     {
       headers: getJWTHeader(user),
+      signal,
     },
   );
   return data.user;
@@ -29,16 +33,20 @@ interface UseUser {
 
 export function useUser(): UseUser {
   const querClient = useQueryClient();
-  const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
-    initialData: getStoredUser,
-    onSuccess: (received: User | null) => {
-      if (!received) {
-        clearStoredUser();
-      } else {
-        setStoredUser(received);
-      }
+  const { data: user } = useQuery(
+    queryKeys.user,
+    ({ signal }) => getUser(user, signal),
+    {
+      initialData: getStoredUser,
+      onSuccess: (received: User | null) => {
+        if (!received) {
+          clearStoredUser();
+        } else {
+          setStoredUser(received);
+        }
+      },
     },
-  });
+  );
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
@@ -48,7 +56,7 @@ export function useUser(): UseUser {
   // meant to be called from useAuth
   function clearUser() {
     querClient.setQueryData(queryKeys.user, null);
-    querClient.removeQueries('user-appointments');
+    querClient.removeQueries([queryKeys.appointments, queryKeys.user]);
   }
 
   return { user, updateUser, clearUser };
